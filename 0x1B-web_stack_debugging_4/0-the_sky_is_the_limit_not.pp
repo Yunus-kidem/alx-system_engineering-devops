e6f82d21d8e90ea341eaf81518eddde6e9af5ce6
unknown <yunuskt363@gmail.com>
2022-02-24 23:14:45 +0300

    last touchs

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -3,3 +3,9 @@ exec {'change limit':
   command  => "sudo sed -i 's/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/g' /etc/default/nginx",
   provider => shell
 }
+
+# restart service
+exec {'restart service':
+  command  => 'sudo service nginx restart',
+  provider => shell
+}
